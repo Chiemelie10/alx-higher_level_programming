f9b86c38698a8a62878bf7feadaff545fc41345c
Chiemelie10 <chiemelieeze@yahoo.com>
2022-11-29 18:49:18 +0000

    Solved task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
-    """ A function that prints string in uppercase """
     for char in str:
         if ord(char) in range(97, 123):
             char = chr(ord(char) - 32)
