b051d0e15a3ce6d82602163affe380e2ce32e724
Chiemelie10 <chiemelieeze@yahoo.com>
2022-11-29 18:07:12 +0000

    Solved task 9, 10, 11

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,3 @@
+def pow(a, b):
+    """ A function that computes a to the power b and return the value """
+    return a ** b
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+def print_last_digit(number):
+    """ Prints the last digit of a number """
+    if number < 0:
+        last_digit = ((-1 * number) % 10)
+    elif number == 0:
+        last_digit = number
+    else:
+        last_digit = number % 10
+    print("{:d}".format(last_digit), end='')
+    return last_digit
