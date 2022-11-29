0c2ca48b181b8479ba24f7a06081e6b9b4ddbd13
Chiemelie10 <chiemelieeze@yahoo.com>
2022-11-29 14:43:45 +0000

    Solved task 7 and 8

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,6 @@
+def islower(c):
+    """ A function that checks for lowercase character"""
+    if ord(c) in range(97, 123):
+        return True
+    else:
+        return False
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+def uppercase(str):
+    """ A function that prints string in uppercase"""
+    for char in str:
+        if ord(char) in range(97, 123):
+            char = chr(ord(char) - 32)
+        print("{:s}".format(char), end="")
+    print()
