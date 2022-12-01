46820ceaa7598840dbeb58b630b236c6f80a2191
Chiemelie10 <chiemelieeze@yahoo.com>
2022-12-01 11:26:11 +0000

    Solved task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,8 +2,8 @@
 
 if __name__ == "__main__":
     """Program that adds 1 and 2"""
-    import add_0
+    from add_0 import add
 
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
