bc2d887e196e4f49eeeaae6b36d7115200fcc828
Chiemelie10 <chiemelieeze@yahoo.com>
2022-12-01 14:30:24 +0000

    Solved task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,15 +8,10 @@ if __name__ == '__main__':
 
     num_of_args = len(sys.argv) - 1
     if num_of_args == 1:
-        print("{} arguement:".format(num_of_args))
+        print("{} argument:".format(num_of_args))
     elif num_of_args < 1:
-        print("{} arguements.".format(num_of_args))
+        print("{} arguments.".format(num_of_args))
     else:
-        print("{} arguements:".format(num_of_args))
-    arg_position = 0
-    for arg in sys.argv:
-        if arg_position == 0:
-            del arg
-        else:
-            print("{}: {}".format(arg_position, arg))
-        arg_position += 1
+        print("{} arguments:".format(num_of_args))
+    for i in range(num_of_args):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
