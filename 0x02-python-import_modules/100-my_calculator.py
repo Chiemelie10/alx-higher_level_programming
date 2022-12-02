55b8ecf7cf0fd88d0ad0fc21deee70e2fa67b48b
Chiemelie10 <chiemelieeze@yahoo.com>
2022-12-02 00:51:49 +0000

    Solved task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    """A program that imports functions from calculator_1.py
+       and handles basic operations.
+    """
+
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    num_of_args = len(sys.argv) - 1
+    if num_of_args != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    operators = {'+': add, '-': sub, '*': mul, '/': div}
+    if sys.argv[2] not in operators.keys():
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    print("{} {} {} = {}\
+          ".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
