9d21ed9081e4a41e5e820b145b6611ef2d857313
Chiemelie10 <chiemelieeze@yahoo.com>
2022-12-03 06:17:54 +0000

    Solved task

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,17 +3,17 @@ def print_reversed_list_integer(my_list=[]):
     """Prints all integers in a list in reverse"""
 
     if my_list == []:
-        print()
-    else:
-        end = len(my_list) - 1
-        first = 0
+        return None
 
-        while first < end:
-            temp = my_list[first]
-            my_list[first] = my_list[end]
-            my_list[end] = temp
-            first += 1
-            end -= 1
+    end = len(my_list) - 1
+    first = 0
 
-        for number in my_list:
-            print("{:d}".format(number))
+    while first < end:
+        temp = my_list[first]
+        my_list[first] = my_list[end]
+        my_list[end] = temp
+        first += 1
+        end -= 1
+
+    for number in my_list:
+        print("{:d}".format(number))
