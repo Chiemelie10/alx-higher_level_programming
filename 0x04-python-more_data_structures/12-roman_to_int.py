8da01b48bec4319815fe7cd944de6ea90908b80b
Chiemelie10 <chiemelieeze@yahoo.com>
2022-12-07 20:25:37 +0000

    Solved task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    """Converts a Roman numeral to an integer"""
+
+    dictionary = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+        }
+    curr = 0
+    prev = 0
+    total = 0
+
+    for i in range(len(roman_string)):
+        curr = dictionary[roman_string[i]]
+        if curr > prev:
+            total = (total + curr) - (2 * prev)
+        else:
+            total += curr
+        prev = curr
+
+    return total
