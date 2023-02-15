d2a2c3d63b42ba79dd7ab1f5cf90a6e27c30b94e
Chiemelie10 <chiemelieeze@yahoo.com>
2023-02-15 18:26:16 +0000

    Task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- This script creates the database hbtn_0d_2 and the user user_0d_2.
+-- Creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Creates a user
+CREATE USER IF NOT EXISTS user_0d_2@localhost
+IDENTIFIED BY 'user_0d_2_pwd';
+-- Grants only SELECT privileges in the hbtn_0d_2 database to the user
+GRANT SELECT
+ON hbtn_0d_2.*
+TO user_0d_2@localhost;
