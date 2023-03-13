12b79fefa783455e1172296a932be72c135f1bee
Chiemelie10 <chiemelieeze@yahoo.com>
2023-03-13 14:04:42 +0000

    Task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const value = process.argv[2];
+
+if (isNaN(value)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = value; i > 0; i--) {
+    console.log('C is fun');
+  }
+}
