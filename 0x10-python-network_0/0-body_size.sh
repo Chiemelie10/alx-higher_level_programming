fc736ef5b892101ccd00358fdd6491f761fd83ef
Chiemelie10 <chiemelieeze@yahoo.com>
2023-03-30 23:36:27 +0000

    Fixed first line

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # This script takes a URL, sends a request to that URL, and displays the size of the body of the response
 curl -s -I "$1" | grep "Content-Length:" | cut -d " " -f2
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # This script takes in a URL, sends a GET request to the URL, and displays the body of the response using the curl command
 curl -sL "$1"
